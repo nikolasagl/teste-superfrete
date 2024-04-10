a4e383e628ea205514d4ad78aec06ea3c2ac3f40
Níkolas Lencioni <nikolasagl@hotmail.com>
2024-04-10 09:27:43 -0300

    adding .env path to dotenv

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as admin from "firebase-admin";
 import {registerUser, userOnCreate} from "./modules/users/index";
-require("dotenv").config();
+require("dotenv").config({path: "../../.env"});
 
 admin.initializeApp();
 
